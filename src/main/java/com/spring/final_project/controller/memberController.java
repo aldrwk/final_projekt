67f99b2a3c36684e45d35a460604d2fb40115955
진관영 <badrow008@gmail.com>
2023-08-29 02:26:26 +0900

    test

--- src/main/java/com/spring/final_project/controller/memberController.java
+++ src/main/java/com/spring/final_project/controller/memberController.java
@@ -1,7 +1,13 @@
 package com.spring.final_project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class memberController {
+
+    @GetMapping("/sign-up")
+    public String signUp(){
+        return "signUp";
+    }
 }
