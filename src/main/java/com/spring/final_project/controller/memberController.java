25c27787d72c50ba5d4ec58d12da0da8213f3a97
진관영 <badrow008@gmail.com>
2023-09-02 19:53:19 +0900

    init : mypage

--- src/main/java/com/spring/final_project/controller/memberController.java
+++ src/main/java/com/spring/final_project/controller/memberController.java
@@ -1,6 +1,7 @@
 package com.spring.final_project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -15,4 +16,11 @@ public class memberController {
     public String login() {
         return "member/login";
     }
+
+    @GetMapping("mypage")
+    public String mypage(String member, Model model) {
+//        Member member로 정보 받아서 ㄱㄱ
+        return "member/mypage";
+    }
+
 }
