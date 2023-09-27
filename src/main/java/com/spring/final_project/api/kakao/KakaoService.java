9218b557f5d7f7dcf4c72b2228c5dd6cfd7c3788
Uri <badrow008@gmail.com>
2023-09-27 17:52:06 +0900

    inite : naver map api & naver sms service

--- src/main/java/com/spring/final_project/api/kakao/KakaoService.java
+++ src/main/java/com/spring/final_project/api/kakao/KakaoService.java
@@ -1,4 +1,4 @@
-package com.spring.final_project.api;
+package com.spring.final_project.api.kakao;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class KakaoService {
--- src/main/resources/static/js/member.js
+++ src/main/resources/static/js/member.js
@@ -22,8 +22,8 @@ $(function () {
     $(".btn-submit.join").click(function (e) {
         console.log("submit btn test")
         if ($(".privateinfo-check,.service-check").prop("checked") == false) {
-            if (!$(".btn-submit:first").prev().hasClass("message")) {
-                $(".btn-submit:first").prev().after(message);
+            if (!$(".btn-submit").eq(1).prev().hasClass("message")) {
+                $(".btn-submit").eq(1).prev().after(message);
             }
             e.preventDefault();
             return false;
@@ -34,7 +34,6 @@ $(function () {
 
     $(".change-password").click(function () {
         $(".info-container").html("").html(pass_change);
-
     });
     var pass_change = "    <div class=\"change-password-container\">\n" +
         "        <p class=\"head-title\">비밀번호 변경</p>\n" +
@@ -51,5 +50,38 @@ $(function () {
         "        </div>\n" +
         "    </div>";
 
+    $(".mobile-area").on("click", ".check-request", function () {
+        const number = $(".mobile_number_check").val();
+        const regPhone = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
+        if (number == "" || !regPhone.test(number)) {
+            if ($(".check-request").next().hasClass("message")) {
+                return false;
+            }
+            if ($(".check-request").next().hasClass("check-area")) {
+                // $(".check-request").next().after().html("");
+                $(".check-request").next().html("");
+                console.log("test");
+            }
+            const message = "<span class=\"message\">올바른 핸드폰 번호를 적어주세요.</span>"
+            $(this).after(message);
+            return false;
+        }
+        if ($(".check-request").next().hasClass("title-input")) {
+            return false;
+        }
+        if($(".check-request").next().hasClass("message")){
+            $(this).next().html("");
+        }
+        $(this).after(auth);
+    });
+    $(".check_number_auth").click(function () {
+
+    });
+
+    var auth = "<div class=\"check-area\" style=\"width: 100%\"> " +
+        "<span class=\"title-input\" >인증 번호</span>" +
+        "<div class=\"check-input\"> <input class=\"text-input check_number\" type=\"text\" name=\"check_number\"\n" +
+        "placeholder=\"인증 번호를 입력해주세요.\">\n" +
+        "<button type=\"button\" class=\"btn btn-submit check_number_auth \">확인 하기</button></div></div>"
 })
 
